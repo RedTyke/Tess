84349ead2ae76a46774d75f333c2f06b9bde3eef
RedTyke <john.mcniffe@hotmail.com>
2017-08-19 06:27:52 +0100

    Initial Commit

---
+++ Tess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tess
+//
+//  Created by John McNiffe on 19/08/2017.
+//  Copyright © 2017 John McNiffe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
